9d3d3f6dba7afb8011b0a03175a007997ffa4a2d
larkincrain <larkincrain@gmail.com>
2017-07-02 19:43:52 +0300

    test program

---
+++ app.js
@@ -0,0 +1,25 @@
+var SerialPort = require("serialport");
+var port = new SerialPort("/dev/ttyAMA0", {
+  baudRate: 19200
+});
+var Printer = require('thermalprinter');
+
+console.log('about to print')
+
+port.on('open',function() {
+
+    console.log('port is open');
+
+    var printer = new Printer(serialPort);
+
+    printer.on('ready', function() {
+        console.log('printer is ready');
+
+        printer
+          .bold(true)
+          .printLine('test message, it works')
+          .print(function() {
+              console.log('done');
+          });
+      });
+  });
